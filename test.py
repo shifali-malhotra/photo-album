ebc1642a802b4d22ab5d4b53ae057e064b01f26f
Shifali Malhotra <83182581+shifali-malhotra@users.noreply.github.com>
2021-04-25 16:01:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -13,27 +13,27 @@ class TestPhotoAlbum(unittest.TestCase):
         sys.argv[1] = 'abc'
         run_program()
         sys.stdout = sys.__stdout__
+
         self.assertEqual(captured_output.getvalue(), 'Album ID must be an integer\n')
 
     def test_album_id_must_be_greater_than_0(self):
-        sys.argv[1] = '1'
         captured_output = io.StringIO()
         sys.stdout = captured_output
         display_photos(0)
         sys.stdout = sys.__stdout__
+
         self.assertEqual(captured_output.getvalue(), 'Album ID must be within 1 and 100\n')
 
     def test_album_id_must_be_less_than_100(self):
-        sys.argv[1] = '1'
         captured_output = io.StringIO()
         sys.stdout = captured_output
         display_photos(101)
         sys.stdout = sys.__stdout__
+
         self.assertEqual(captured_output.getvalue(), 'Album ID must be within 1 and 100\n')
 
     @responses.activate
     def test_album_id_for_id_of_3(self):
-        sys.argv[1] = '3'
         responses.add(
             responses.GET, 'https://jsonplaceholder.typicode.com/photos?albumId=3',
             json=[
@@ -54,12 +54,11 @@ class TestPhotoAlbum(unittest.TestCase):
         sys.stdout = sys.__stdout__
 
         self.assertEqual(captured_output.getvalue(), '[101] incidunt alias vel enim\n')
-        self.assertEqual(len(responses.calls), 1)\
+        self.assertEqual(len(responses.calls), 1)
 
 
     @responses.activate
     def test_bad_response(self):
-        sys.argv[1] = '3'
         responses.add(
             responses.GET, 'https://jsonplaceholder.typicode.com/photos?albumId=3',
             status=520
@@ -75,5 +74,4 @@ class TestPhotoAlbum(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
+    
