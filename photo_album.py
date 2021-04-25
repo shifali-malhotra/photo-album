a5a8012ac6b02333f0b12e3ea12ca75aa892204d
Shifali Malhotra <83182581+shifali-malhotra@users.noreply.github.com>
2021-04-25 15:28:44 -0500

    Add files via upload

---
+++ photo_album.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import requests
+
+def run_program():
+    if sys.argv[1].isdigit():
+        album_id = int(sys.argv[1])
+        display_photos(album_id)
+    else:
+        print('Album ID must be an integer')
+
+def display_photos(album_id):
+    if album_id < 1 or album_id > 100:
+        print('Album ID must be within 1 and 100')
+    else:
+        response = requests.get(f"https://jsonplaceholder.typicode.com/photos?albumId={album_id}")
+
+        if response.status_code == 200:
+            for photo in response.json():
+                print(f"{[photo.get('id')]} {photo.get('title')}")
+        else:
+            print("Bad response, try again.")
+
+run_program()
---
+++ test.py
@@ -0,0 +1,79 @@
+import unittest
+import sys
+import io
+import responses
+
+from photo_album import display_photos, run_program
+
+
+class TestPhotoAlbum(unittest.TestCase):
+    def test_album_id_must_be_integer(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        sys.argv[1] = 'abc'
+        run_program()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), 'Album ID must be an integer\n')
+
+    def test_album_id_must_be_greater_than_0(self):
+        sys.argv[1] = '1'
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        display_photos(0)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), 'Album ID must be within 1 and 100\n')
+
+    def test_album_id_must_be_less_than_100(self):
+        sys.argv[1] = '1'
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        display_photos(101)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), 'Album ID must be within 1 and 100\n')
+
+    @responses.activate
+    def test_album_id_for_id_of_3(self):
+        sys.argv[1] = '3'
+        responses.add(
+            responses.GET, 'https://jsonplaceholder.typicode.com/photos?albumId=3',
+            json=[
+              {
+                "albumId": 3,
+                "id": 101,
+                "title": "incidunt alias vel enim",
+                "url": "https://via.placeholder.com/600/e743b",
+                "thumbnailUrl": "https://via.placeholder.com/150/e743b"
+              }
+            ],
+            status=200
+        )
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        display_photos(3)
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue(), '[101] incidunt alias vel enim\n')
+        self.assertEqual(len(responses.calls), 1)\
+
+
+    @responses.activate
+    def test_bad_response(self):
+        sys.argv[1] = '3'
+        responses.add(
+            responses.GET, 'https://jsonplaceholder.typicode.com/photos?albumId=3',
+            status=520
+        )
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        display_photos(3)
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue(), 'Bad response, try again.\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
